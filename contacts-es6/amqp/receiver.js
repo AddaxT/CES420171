bf05bf4914ff9723ae9e1f5e8ba7426025a52c8d
Luis Carlos Sanchez Gonzalez <bipter@gmail.com>
2017-06-26 22:18:41 -0500

    Solved receiver connection issue

--- contacts-es6/amqp/receiver.js
+++ contacts-es6/amqp/receiver.js
@@ -8,16 +8,20 @@ class Receiver {
     }
 
     receiveMessage(messageHandler) {
+        let _self = this;
         amqp.connect(this.host).then(function (connection) {
             return connection.createChannel().then(function (channel) {
-                var ok = channel.assertQueue(this.queue, { durable: false });
+                var ok = channel.assertQueue(_self.queue, { durable: false });
 
                 ok = ok.then(function (_qok) {
-                    return channel.consume(this.queue, function (message) {
+                    return channel.consume(_self.queue, function (message) {
                         messageHandler(message);
+                        connection.close();
                     }, { noAck: true });
                 });
-            }).finally(function () { connection.close(); });
+            });
         }).catch(console.warn);
     }
 }
+
+exports.Receiver = Receiver;
